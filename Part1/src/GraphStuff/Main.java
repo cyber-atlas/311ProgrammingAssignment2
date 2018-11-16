fb8697e4ea6926949a15944904797bf07ce52689
ads97 <ads97@iastate.edu>
2018-11-16 07:03:49 -0600

    Working with Yussef now. We have an implementation of both graph and image classes. Wgraph right now works for v2v for the first path. Need to work on Dijikstras

--- Part1/src/GraphStuff/Main.java
+++ Part1/src/GraphStuff/Main.java
@@ -1,28 +1,31 @@
-package GraphStuff;
-
-import java.io.FileNotFoundException;
-import java.net.URISyntaxException;
-
-public class Main {
-
-    public static void main(String[] args) throws FileNotFoundException, URISyntaxException {
-
-        //Initializes a new graph object
-        WGraph2 g = new WGraph2("test2.txt");
-
-        g.parseFile();
-
-        System.out.println("Checking the edgetable");
-        System.out.println(g.edgeTable);
-        //Prints the weight of each key in the edgeTable
-        for(String key: g.edgeTable.keySet()){
-            System.out.println(key + " " + g.edgeTable.get(key).getWeight());
-        }
-
-        System.out.println("Checking the graphRepresentation");
-        System.out.println(g.graphMap);
-
-
-
-    }
-}
+//package GraphStuff;
+//
+//import java.io.FileNotFoundException;
+//import java.net.URISyntaxException;
+//
+//public class Main {
+//
+//    public static void main(String[] args) throws FileNotFoundException, URISyntaxException {
+//
+//        //Initializes a new graph object
+//        WGraph2 g = new WGraph2("test2.txt");
+//
+//        g.parseFile();
+//
+//        System.out.println("Checking the edgetable");
+//        System.out.println(g.edgeTable);
+//        //Prints the weight of each key in the edgeTable
+//        System.out.println("Printing the edgeTable key & weight");
+//        for(String key: g.edgeTable.keySet()){
+//            System.out.println("key " + key + " " + "value " + g.edgeTable.get(key).getWeight());
+//        }
+//
+//        System.out.println("Checking the graphRepresentation");
+//        System.out.println(g.graphMap);
+//
+//        System.out.print(g.V2V(23, 55, 14, 10));
+//
+//
+//
+//    }
+//}
--- Part1/src/GraphStuff/WGraph.java
+++ Part1/src/GraphStuff/WGraph.java
@@ -1,15 +1,18 @@
 package GraphStuff;
 
-import java.io.*;
-import java.lang.reflect.Array;
+/**
+ * TODO Let's try this again without sipping dumbfuck juice
+ */
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.net.URISyntaxException;
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Scanner;
+import java.util.*;
 
-public class WGraph {
+//public class WGraph2 {
+    public class WGraph {
 
     //Instance variables
 
@@ -26,45 +29,67 @@ public class WGraph {
     private int numEdges;
 
 
-    //Making a Hashset to hold the Edges
-    HashSet<Edge> edgeSet;
-    //Making a Hashmap of linked lists to hold the adjacency list of the graph
-    HashMap<String, ArrayList<String>> graphMap;
+//    Pair<String, Integer> edge;
 
+    private Hashtable<String, Integer> hash;
+
+
+
+    //Making a Hashset to hold the Edges
+    private HashSet<Edge> edgeSet;
+    //Making a Hashmap of linked lists to hold the adjacency list of the graph
+    protected HashMap<String, ArrayList<String>> graphMap;
+
+    //Making a HashTable to hold the edges...
+    protected Hashtable<String, Edge> edgeTable;
+
+
+    //Hashset to hold whether or not we visited a node
+    protected HashSet<String> visitedHashSet;
+    //Hashmap to hold distances
+    protected HashMap<String, Integer> distanceMap;
+
+    protected HashMap<String, String> previousMap;
+
+    protected ArrayList<String> vertexList = new ArrayList<>();
 
     /**
      * Constructor for WGraph
      * @param FName the filename of the file in the same directory
      */
+//    public WGraph2(String FName){
     public WGraph(String FName){
         //Saves the filename of the file
         fileName = FName;
+        parseFile();
     }
 
 
-    private void parseFile() throws FileNotFoundException, URISyntaxException {
-//        fileIn  = new FileReader(fileName);
-         //Initalize the hashmap and hashset
-        edgeSet = new HashSet<Edge>();
-        graphMap = new HashMap<String, ArrayList<String>>();
-        URL path = ClassLoader.getSystemResource(fileName);
-        File f  = new File(fileName);
-        Scanner s = new Scanner(f);
+    public void parseFile() {
         //Initalize the hashmap and hashset
         edgeSet = new HashSet<Edge>();
         graphMap = new HashMap<String, ArrayList<String>>();
+        File f  = new File(fileName);
+        Scanner s = null;
+        try {
+            s = new Scanner(f);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        //Initalize the hashmap that holds the graph adjacency list
+        graphMap = new HashMap<String, ArrayList<String>>();
+        //Try to have a hashTable of edges, the key would be the edgename, the value would be the edge object
+        edgeTable = new Hashtable<String, Edge>();
+
         //The first line is going to be the number of vertices in the graph
         numVertices = s.nextInt();
         System.out.println(numVertices);
         //The second is going to be the number of edges in the graph
         numEdges = s.nextInt();
         System.out.println(numEdges);
-        System.out.println(String.valueOf(numVertices)+String.valueOf(numEdges));
+
 
         //So now we know that we can get the input properly and that we ignore the new line character
-
-        //Getting the source
-
         //A scanner should only have 5 ints on the line seperated by spaces
         while (s.hasNextLine()){
             //Make the source vertex
@@ -76,37 +101,188 @@ public class WGraph {
             //Save the weight of the edge
             int weight = s.nextInt();
 
-            String sourceXYString  = String.valueOf(sourceX) + String.valueOf(sourceY);
-            String destXYString = String.valueOf(destX) + String.valueOf(destY);
 
-            //TODO store the edge
-            Edge tempEdge = new Edge(sourceXYString, destXYString, weight);
+            String sourceXYString  = String.valueOf(sourceX) + ',' + String.valueOf(sourceY);
+            String destXYString = String.valueOf(destX) + ',' + String.valueOf(destY);
 
-            //Adds the temp edge to the hashset
-            edgeSet.add(tempEdge);
+            vertexList.add(sourceXYString);
+            vertexList.add(destXYString);
 
-            //TODO create the hashmap of adjacies
-            //TODO figure out another hashmap contstructor thing
-//            graphMap(sourceXYString, so,
-//                    (String.valueOf(sourceX) + String.valueOf(sourceY)));
+            System.out.println("source: " + sourceXYString + " dest " + destXYString + " weight " + weight);
 
+            //Let's try this again adding it to a hashtable with the full edgename as the key, new edge as value
+            edgeTable.put((sourceXYString+","+destXYString), new Edge(sourceXYString, destXYString, weight));
 
+            //If this entry in the hashmap does not exist, add it and initialize the arraylist
+            if (!(graphMap.containsKey(sourceXYString))){
+                graphMap.put(sourceXYString, new ArrayList<String>());
+            }
 
+            //Gets the key of the sourceXY string and adds the destination string to the arraylist of children
+            graphMap.get(sourceXYString).add(destXYString);
+        }
+    }
+
+    //TODO implement V2V
+    //TODO look into whether or not we need to mark as visited or not
+    /** Given vertices u and v, ﬁnd the shortest path to from u to v.
+     pre: ux, uy, vx, vy are valid coordinates of vertices u and v
+        in the graph
+     post: return arraylist contains even number of integers,
+        for any even i,
+        i-th and i+1-th integers in the array represent
+        the x-coordinate and y-coordinate of the i/2-th vertex
+        in the returned path (path is an ordered sequence of vertices) */
+
+    public ArrayList<Integer> V2V(int ux, int uy, int vx, int vy){
+
+        String sourcenode = String.valueOf(ux) + "," + String.valueOf(uy);
+        String destnode = String.valueOf(vx) + "," + String.valueOf(vy);
+
+        ArrayList<Integer> returnList = new ArrayList<Integer>();
+
+       visitedHashSet = new HashSet<>();
+        //Hashmap to hold distances
+       distanceMap = new HashMap<>();
+       previousMap = new HashMap<>();
+
+        //Make a new priority queue
+        PriorityQueue priorityQueue = new PriorityQueue(comparator);
+
+        //loops through all the verteces and adds them to the priority queue
+        for(String v:vertexList) {
+            priorityQueue.add(v);
         }
 
-        //TODO get the next line
-        //TODO Parse the ints from the string/line
-        //
+        //Remove source, set it's distance to 0 then read bc PQ only updates on insertion/deletion
+        priorityQueue.remove(sourcenode);
+        distanceMap.put(sourcenode, 0);
+        priorityQueue.add(sourcenode);
+
+        while(!(priorityQueue.isEmpty())){
+            //pops the lowest vertex off the priority queue
+           String tempNode = (String)priorityQueue.poll();
+
+           System.out.println( "tempNode" + tempNode);
+
+           System.out.println("tempnode " + tempNode + " dest " + destnode );
+
+           //TODO check if the node we popped is the node we need
+            if (tempNode.equals(destnode)){
+
+                //TODO invert the prev list, and convert them to ints
+//                return  returnList;
+                return getParent(tempNode);
+            }
+
+           //If tempNode does not have a child, continue the loop
+           if ((graphMap.get(tempNode)) == null){
+               continue;
+           }
+
+           //For each neighbor of ourNode
+           for(String child: graphMap.get(tempNode)){
+               //The weight of the edge
+              int distToAdd = edgeTable.get(tempNode+ "," + child).getWeight();
+              //TODO if not in our distance map, then we set this to 0 I am assuming
+               //The previous weight of the node
+               int previousWeight = (distanceMap.get(tempNode) == null)? 0 : distanceMap.get(tempNode);
+               int newDist = distToAdd + previousWeight;
+               //Set the previous child node value to max int if it is not in the distanceMap or set it to prev value
+               int prevChildVal = (distanceMap.get(child) == null)? Integer.MAX_VALUE : distanceMap.get(child);
+               //If the new distance is less than the old update the child's weight and the previous map
+               if(newDist < prevChildVal) {
+                   distanceMap.put(child, (distToAdd + previousWeight));
+                   previousMap.put(child,tempNode);
+               }
+           }
+        }
+
+        //TODO return a reverse version of previous.
+        //reverse traverse prev, and return vertex in order
+        /*
+        while(not null){
+            go through previous, and for every previous, add that to the array and go through it
+        } */
+        return returnList;
     }
 
-    public static void main(String[] args) throws FileNotFoundException, URISyntaxException {
 
-        System.out.println("Hello World!");
-        WGraph g = new WGraph("test2.txt");
-//        WGraph g = new WGraph("C:\\Users\\ruski\\Documents\\ProgrammingAssignment2\\Part1\\src\\GraphStuff\\test2.txt");
-        g.parseFile();
+    //TODO V2S instead of checking if the chld node is the node we need, check if it is int the set of nodes that we need, if it's in the hashset
+
+
+
+    Comparator<String> comparator = new Comparator<String>() {
+        @Override
+        public int compare(String node1, String node2) {
+
+            //If the value is null in the distancemap, then say that it is the biggest int since it's not been traversed
+            int n1 = (distanceMap.get(node1) == null) ? Integer.MAX_VALUE : distanceMap.get(node1);
+
+            int n2 = (distanceMap.get(node2) == null) ? Integer.MAX_VALUE : distanceMap.get(node2);
+
+            //If the nodes are equal it will be 0
+            //If n1 - n2 is negative then n2 is bigger else n1
+            return n1 - n2;
+        }
+    };
+
+    //TODO optimize this and finish this
+    private List<String> reverse(String start) {
+        Stack<String> s = new Stack<String>();
+        String cur = start;
+        while(cur != null) {
+            s.push(cur);
+            cur = previousMap.get(cur);
+        }
+        List<String> s2 = new ArrayList<String>();
+        while(!s.empty()) {
+            s2.add(s.pop());
+        }
+        return s2;
+    }
+
+    private ArrayList<Integer> getParent(String endNode){
+        ArrayList<Integer> returnList = new ArrayList<>();
+
+//        String nodeBeforeCur = previousMap.get(endNode);
+        String nodeBeforeCur = (endNode);
+
+
+        //TODO find a way to deal with it becoming an int. Ex its 1122 rn instead of 11 22
+        //While the node before actually has a parent, null after it grabs the source which has no parent
+//        while(nodeBeforeCur != null){
+            do{
+            //Grabs the int value of the previous node
+
+            String[] tmpList = nodeBeforeCur.split(",");
+
+//            for(String s : tmpList){
+//                returnList.add(Integer.parseInt(s));
+//            }
+
+            //Since they get rerversed later, adding them in reverse order
+            for(int i = tmpList.length-1; i>=0; i--){
+                String s = tmpList[i];
+                System.out.println(s);
+                returnList.add(Integer.parseInt(s));
+            }
+
+//            returnList.add(Integer.valueOf(nodeBeforeCur.split(",")));
+           //Grabs the next parent node
+            nodeBeforeCur = previousMap.get(nodeBeforeCur);
+        } while (nodeBeforeCur != null);
+
+        //Reverses the list before it is returns
+        Collections.reverse(returnList);
+        return returnList;
+        }
+
+
+
+
+
 
     }
 
 
-}
